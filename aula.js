eed4fa8a53e98814cac6c331b0d9dda4863794f6
Denis <denis_leme@icloud.com>
2022-11-27 17:18:16 -0300

    feat:CalculoGasolina

---
+++ aula.js
@@ -0,0 +1,10 @@
+const precoCombustivel = 5.26;
+const kmPorLitros = 9.4;
+const DistanciaEmKm = 1580;
+
+const litrosconsumidos = DistanciaEmKm  / kmPorLitros
+const valorgasto =  litrosconsumidos * precoCombustivel;
+
+
+console.log(valorgasto);
+ 
